c1e710d5052960a6070b5ea4e8caa83a32727c1d
felipeOliveira-1 <contato@felipesilva.tec>
2023-02-15 18:37:56 -0300

    update

--- prompt_generator.py
+++ prompt_generator.py
@@ -1,22 +1,65 @@
 import os
 import openai
+import sys
 
 # Set the OpenAI API key
-openai.api_key = ""
+openai.api_key = "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"
 
 def get_user_input():
+    option = input("Please choose an option:\n1. Simulate an expert\n2. Challenge the conventional narrative\n3. Use unconventional prompts\n4. Ultra-Brainstormer\n5. Add in human-written techniques\n6. Write from different perspectives\n7. Write in different styles or tones, such as satire or irony\n")
     desired_prompt = input("\nPlease enter the prompt you want GPT-3 to respond to: ")
     role = input("\nWhat role should GPT-3 act as in the prompt? (e.g. a teacher, a scientist) ")
-    additional_info = input("\nAny additional information or context to add to the prompt? \nExample:\nPrompt: Write an essay about the importance of exercise. \nAdditional information/context: Your audience is a group of sedentary office workers who are looking for ways to improve their physical health and reduce stres\n")
+    additional_info = input("\nAny additional information or context to add to the prompt? ")
 
-    return desired_prompt, role, additional_info
+    return option, desired_prompt, role, additional_info
 
-def create_prompt(desired_prompt, role, additional_info):
-    example = f"Act as a {role}. {additional_info}. {desired_prompt}\n"
-    print("Here's an example of a prompt you could use:")
-    print(example)
+def get_user_input():
+    option = input("Please choose an option:\n1. Simulate an expert\n2. Challenge the conventional narrative\n3. Use unconventional prompts\n4. Ultra-Brainstormer\n5. Add in human-written techniques\n6. Write from different perspectives\n7. Write in different styles or tones, such as satire or irony\n")
 
-    prompt = f"Act as {role}. {additional_info}. {desired_prompt}"
+    if option == '1':
+        desired_prompt = input("\nPlease enter the prompt you want GPT-3 to respond to: ")
+        role = "an expert"
+        additional_info = ""
+
+    elif option == '2':
+        desired_prompt = input("\nPlease enter the prompt you want GPT-3 to respond to: ")
+        role = input("\nWhat role should GPT-3 act as in the prompt? (e.g. a teacher, a scientist) ")
+        additional_info = input("\nAny additional information or context to add to the prompt? ")
+
+    elif option == '3':
+        desired_prompt = input("\nPlease enter the prompt you want GPT-3 to respond to: ")
+        role = "a creative writer"
+        additional_info = ""
+
+    elif option == '4':
+        desired_prompt = input("\nPlease enter the prompt you want GPT-3 to respond to: ")
+        role = "an idea generator"
+        additional_info = input("\nAny additional information or context to add to the prompt? ")
+
+    elif option == '5':
+        desired_prompt = input("\nPlease enter the prompt you want GPT-3 to respond to: ")
+        role = "a persuasive writer"
+        additional_info = input("\nAny additional information or context to add to the prompt? ")
+
+    elif option == '6':
+        desired_prompt = input("\nPlease enter the prompt you want GPT-3 to respond to: ")
+        role = "a group of characters with different backgrounds or viewpoints"
+        additional_info = input("\nPlease provide information about the different perspectives you want to include: ")
+
+    elif option == '7':
+        desired_prompt = input("\nPlease enter the prompt you want GPT-3 to respond to: ")
+        role = "a satirical or ironic writer"
+        additional_info = ""
+
+    else:
+        print("Invalid option selected.")
+        return None
+
+    return option, desired_prompt, role, additional_info
+
+
+    print(prompt)
+    sys.stdout.flush() # add this line to flush the output buffer
     return prompt
 
 def generate_response(prompt):
@@ -34,8 +77,15 @@ def generate_response(prompt):
     return generated_prompt
 
 def generate_prompt():
-    desired_prompt, role, additional_info = get_user_input()
-    prompt = create_prompt(desired_prompt, role, additional_info)
+    option, desired_prompt, role, additional_info = get_user_input()
+
+    prompt = f"Act as {role}. {additional_info}. {desired_prompt}"
+    print("Here's an example of a prompt you could use:")
+    print(prompt)
+
     generated_prompt = generate_response(prompt)
     return generated_prompt
+
+
+# Call the generate_prompt() function and print the generated prompt
 print(generate_prompt())
