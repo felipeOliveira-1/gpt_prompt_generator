b285773bcd931891eaaf00eb530480fb8de5567a
felipeOliveira-1 <contato@felipesilva.tec>
2023-02-15 17:35:19 -0300

    update

--- prompt_generator.py
+++ prompt_generator.py
@@ -2,23 +2,24 @@ import os
 import openai
 
 # Set the OpenAI API key
-openai.api_key = "API-key"
+openai.api_key = ""
 
-# Define the generate_prompt function
-def generate_prompt():
-    # Get input from user
+def get_user_input():
     desired_prompt = input("Please enter the prompt you want GPT-3 to respond to: ")
     role = input("What role should GPT-3 act as in the prompt? (e.g. a teacher, a scientist) ")
     additional_info = input("Any additional information or context to add to the prompt? Ex. Prompt: Write an essay about the importance of exercise. Additional information/context: Your audience is a group of sedentary office workers who are looking for ways to improve their physical health and reduce stres: ")
 
-    # Provide an example for each input
+    return desired_prompt, role, additional_info
+
+def create_prompt(desired_prompt, role, additional_info):
+    example = f"Act as a {role}. {additional_info}. {desired_prompt}\n"
     print("Here's an example of a prompt you could use:")
-    print(f"Act as a {role}. {additional_info}. {desired_prompt}\n")
+    print(example)
 
-    # Create the prompt using the user input
     prompt = f"Act as {role}. {additional_info}. {desired_prompt}"
+    return prompt
 
-    # Generate the prompt using the OpenAI API
+def generate_response(prompt):
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
@@ -29,10 +30,13 @@ def generate_prompt():
         presence_penalty=0,
         stop=["\n", ".", "!", "?"]
     )
-
-    # Extract the generated prompt from the OpenAI API response
     generated_prompt = response.choices[0].text.strip()
+    return generated_prompt
 
+def generate_prompt():
+    desired_prompt, role, additional_info = get_user_input()
+    prompt = create_prompt(desired_prompt, role, additional_info)
+    generated_prompt = generate_response(prompt)
     return generated_prompt
 
 generate_prompt()
