47710a251f09659d81d11df2249a0c6f699dc69f
felipeOliveira-1 <contato@felipesilva.tec>
2023-02-16 07:47:06 -0300

    update

--- prompt_generator.py
+++ prompt_generator.py
@@ -10,8 +10,27 @@ def get_user_input():
 
     if option == '1':
         desired_prompt = input("\nPlease enter the prompt you want GPT-3 to respond to: ")
-        role =  input("\nWhat role should GPT-3 act as in the prompt? (e.g. a teacher, a scientist) ")
+        role_choice = input("\nPlease choose a role for GPT-3 to act as in the prompt:\n1. A teacher\n2. A scientist\n3. An artist\n4. An entrepreneur\n5. A journalist\n6. Enter your own role\nEnter a number: ")
+    
+        while role_choice not in ['1', '2', '3', '4', '5', '6']:
+            role_choice = input("Invalid role choice. Please enter a valid number or type your own role: ")
+    
+        if role_choice == '1':
+            role = "a teacher"
+        elif role_choice == '2':
+            role = "a scientist"
+        elif role_choice == '3':
+            role = "an artist"
+        elif role_choice == '4':
+            role = "an entrepreneur"
+        elif role_choice == '5':
+            role = "a journalist"
+        elif role_choice == '6':
+            role = input("Please enter the role you want GPT-3 to act as in the prompt: ")
+        
         additional_info = input("\nAny additional information or context to add to the prompt? ")
+
+    
 
     elif option == '2':
         topic = input("\nEnter a Topic: ")
@@ -48,7 +67,19 @@ def get_user_input():
 
     elif option == '7':
         desired_prompt = input("\nPlease enter the prompt you want GPT-3 to respond to: ")
-        role = "a satirical or ironic writer"
+        style_choice = input("\nPlease choose a style or tone:\n1. Satire\n2. Irony\n3. Enter your own style or tone\nEnter a number: ")
+        
+        while style_choice not in ['1', '2', '3']:
+            style_choice = input("Invalid choice. Please enter a valid number or type your own style or tone: ")
+        
+        if style_choice == '1':
+            style = "satire"
+        elif style_choice == '2':
+            style = "irony"
+        elif style_choice == '3':
+            style = input("Please enter the style or tone you want GPT-3 to use in the prompt: ")
+        
+        role = f"a {style} writer"
         additional_info = ""
 
     else:
@@ -82,8 +113,8 @@ def generate_response(prompt):
 def generate_prompt():
     option, desired_prompt, role, additional_info = get_user_input()
 
-    prompt = f"Act as {role}. {desired_prompt}, {additional_info}. "
-    print("Here's an example of a prompt you could use:")
+    prompt = f"Act as {role}. {desired_prompt}. {additional_info}."
+    print("\nHere's an example of a prompt you could use:\n")
     print(prompt)
 
     generated_prompt = generate_response(prompt)
